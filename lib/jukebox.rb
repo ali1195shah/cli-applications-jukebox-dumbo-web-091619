65207044e06e2cb7ce71f6b418756bdb42aa058c
Ali Shah <ali1195shah@gmail.com>
2019-09-04 23:20:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -77,8 +77,7 @@ def run(songs)
       puts "Please enter a command:"
       input = gets.strip
     end
-    puts "Please enter a command:"
-    input = gets.strip
+    
   end
   exit_jukebox
 end
